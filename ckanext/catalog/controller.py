9d5b843bbc166a6a5ae8e950c79ae02ca78545f3
John Glover <j@johnglover.net>
2011-06-29 11:12:52 +0200

    Override search to strip 'http://' from query string

--- ckanext/catalog/controller.py
+++ ckanext/catalog/controller.py
@@ -3,18 +3,17 @@ CKAN Catalog Extension
 """
 from logging import getLogger
 log = getLogger(__name__)
-import datetime
-import re
 
 from pylons.i18n import _
 from pylons.decorators import jsonify
 from pylons import request, tmpl_context as c
 from pylons import config, cache
-from ckan.lib.base import BaseController, response, render, abort, h
+from ckan.lib.base import BaseController, response, render, abort, h, g
 from ckan.lib.base import etag_cache, response, redirect, gettext
 from ckan.lib.package_saver import PackageSaver, ValidationException
 from ckan.lib.cache import proxy_cache
-from ckan.controllers.package import PackageController, autoneg_cfg
+from ckan.lib.search import query_for, SearchError
+from ckan.controllers.package import PackageController, autoneg_cfg, search_url
 from ckan.lib.navl.validators import (
     ignore_missing, not_empty, ignore, keep_extras
 )
@@ -98,3 +97,77 @@ class CatalogController(PackageController):
 
     def _check_data_dict(self, data_dict):
         return
+
+    def search(self):        
+        if not self.authorizer.am_authorized(c, model.Action.SITE_READ, model.System):
+            abort(401, _('Not authorized to see this page'))
+        q = c.q = request.params.get('q') # unicode format (decoded from utf8)
+
+        # hack q to get url search working:
+        # - remove 'http://'
+        q = q.replace('http://', '') if q else None
+
+        c.open_only = request.params.get('open_only')
+        c.downloadable_only = request.params.get('downloadable_only')
+        c.query_error = False
+        try:
+            page = int(request.params.get('page', 1))
+        except ValueError, e:
+            abort(400, ('"page" parameter must be an integer'))
+        limit = 20
+        query = query_for(model.Package)
+
+        # most search operations should reset the page counter:
+        params_nopage = [(k, v) for k,v in request.params.items() if k != 'page']
+        
+        def drill_down_url(**by):
+            params = list(params_nopage)
+            params.extend(by.items())
+            return search_url(set(params))
+        
+        c.drill_down_url = drill_down_url 
+        
+        def remove_field(key, value):
+            params = list(params_nopage)
+            params.remove((key, value))
+            return search_url(params)
+
+        c.remove_field = remove_field
+        
+        def pager_url(q=None, page=None):
+            params = list(params_nopage)
+            params.append(('page', page))
+            return search_url(params)
+
+        try:
+            c.fields = []
+            for (param, value) in request.params.items():
+                if not param in ['q', 'open_only', 'downloadable_only', 'page'] \
+                        and len(value) and not param.startswith('_'):
+                    c.fields.append((param, value))
+
+            query.run(query=q,
+                      fields=c.fields,
+                      facet_by=g.facets,
+                      limit=limit,
+                      offset=(page-1)*limit,
+                      return_objects=True,
+                      filter_by_openness=c.open_only,
+                      filter_by_downloadable=c.downloadable_only,
+                      username=c.user)
+                       
+            c.page = h.Page(
+                collection=query.results,
+                page=page,
+                url=pager_url,
+                item_count=query.count,
+                items_per_page=limit
+            )
+            c.facets = query.facets
+            c.page.items = query.results
+        except SearchError, se:
+            c.query_error = True
+            c.facets = {}
+            c.page = h.Page(collection=[])
+        
+        return render('package/search.html')
